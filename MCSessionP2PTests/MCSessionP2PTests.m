d55e71601183afbf21fdd165042146f41653c5e6
Marco Abundo <shrtlist@gmail.com>
2013-11-30 11:29:35 -0500

    Initial commit

---
+++ MCSessionP2PTests/MCSessionP2PTests.m
@@ -0,0 +1,34 @@
+//
+//  MCSessionP2PTests.m
+//  MCSessionP2PTests
+//
+//  Created by Marco Abundo on 11/23/13.
+//  Copyright (c) 2013 shrtlist. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MCSessionP2PTests : XCTestCase
+
+@end
+
+@implementation MCSessionP2PTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
